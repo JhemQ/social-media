b91d917b2f113764f9638a4666a7a81c8146b0d1
msantos9-dev <112072332+msantos9-dev@users.noreply.github.com>
2022-10-31 20:46:02 +0800

    header lay-out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import Header from './components/Header';
 import PageRender from './PageRender';
+
 
 function App() {
   return (
+
     <Router>
       <input type="checkbox" id='theme' />
       <div className="App">
         <div className="main">
+            <Header/>
           <Routes>
             <Route exact path='/:page' component={PageRender} element={<PageRender/>} />
             <Route exact path='/:page/:id' component={PageRender} element={<PageRender/>} />
